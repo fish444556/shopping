0b867d71bb7c5a76f4bdc07db11d9957da741073
fish444556 <jialinl@mtu.edu>
2017-11-20 10:58:40 -0500

    login & logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ConnectedBackComponent } from './connected-back/connected-back.component';
 import { GetDataService } from './get-data.service';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConnectedBackComponent
+    ConnectedBackComponent,
+    NavComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'cb',
+        component: ConnectedBackComponent
+      },
+      {
+        path:'login',
+        component:LoginComponent
+      }
+    ])
   ],
-  providers: [GetDataService],
+  providers: [GetDataService, UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { Observable } from "rxjs/Observable";
+
+@Injectable()
+export class UserService {
+  registerUrl = 'http://localhost:8088/api/register';
+  loginUrl = 'http://localhost:8088/api/user';
+  userStatus: number = 0;
+
+  constructor(private http: Http) { }
+
+  private _showNavBar: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
+  public showNavBarEmitter: Observable<boolean> = this._showNavBar.asObservable();
+
+  userRegister(registerData){
+    return this.http.post(this.registerUrl, registerData);
+  }
+
+  userLogin(loginData) {
+    return this.http.post(this.loginUrl, loginData);
+  }
+
+  showNavBar(ifShow: boolean) {
+    this._showNavBar.next(ifShow);
+  }
+
+}
